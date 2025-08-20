3c749d1407284d025e09226d9a4da900cf91b611
JTestore <thiagomoraiscp@gmail.com>
2025-08-19 23:03:14 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Arial, sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>Dashboard de Parceria de Estágio</h1>
+      <p>Bem-vindo ao projeto de estágio da Compliance!</p>
+    </div>
+  );
+}
+
+export default App;
